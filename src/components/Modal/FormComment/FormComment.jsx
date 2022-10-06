3c27c8ed491cc70dc7d98d45982805a9e8349a6b
HeleneElena <heleneebert1@gmail.com>
2022-10-06 10:55:02 +0200

    commit2

--- src/components/Modal/FormComment/FormComment.jsx
+++ src/components/Modal/FormComment/FormComment.jsx
@@ -1,26 +1,37 @@
 import style from './FormComment.module.css';
 import {Text} from '../../../UI/Text';
-import {useRef, useContext, useState} from 'react';
+import {useContext, useState} from 'react';
 import {authContext} from './../../../context/authContext';
+import {CommentContext} from '../../../context/commentContext';
+import {useStore} from 'react-redux';
 
 export const FormComment = () => {
+  const store = useStore();
   const [formOpen, setFormOpen] = useState(false);
   const {auth} = useContext(authContext);
-  const textareaRef = useRef(null);
+  const {value, setValue} = useContext(CommentContext);
 
   const userComment = e => {
     e.preventDefault();
-    console.log(textareaRef.current.value);
+    console.log(value);
   };
 
   const handleFormOpen = () => {
     setFormOpen(true);
   };
 
+  const handleChange = e => {
+    setValue(e.target.value);
+  };
+
   return formOpen ? (
         <form className={style.form} onSubmit={userComment}>
           <Text As='h3' size={14} tsize={18}>Имя авторизованного пользователя: {auth.name}</Text>
-          <textarea className={style.textarea} ref={textareaRef}></textarea>
+          <textarea 
+            className={style.textarea} 
+            value={value}
+            onChange={handleChange}
+          ></textarea>
           <button className={style.btn}>Отправить</button>
         </form>
       ) : (
