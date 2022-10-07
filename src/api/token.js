1c7358eb7fe832a77c04948e84c824fb3bcd8ee7
HeleneElena <heleneebert1@gmail.com>
2022-10-07 09:44:56 +0200

    fix: Redux

--- src/api/token.js
+++ src/api/token.js
@@ -1,17 +1,13 @@
-export const setToken = token => {
+export const setToken = (token) => {
   localStorage.setItem('bearer', token);
 };
 
 export const getToken = () => {
-  let token = '';
-  if (location.pathname.includes('/auth')) {
-    token = new URLSearchParams(location.hash.substring(1)).get('access_token');
-    setToken(token);
-  }
+  let token = localStorage.getItem('bearer') || '';
 
-  if (localStorage.getItem('bearer')) {
-    token = localStorage.getItem('bearer');
-    setToken(localStorage.getItem('bearer'));
+  if (!token && location.pathname.includes('/auth')) {
+    token = new URLSearchParams(location.hash.substring(1)).get('access_token') || '';
+    token && setToken(token);
   }
 
   return token;
